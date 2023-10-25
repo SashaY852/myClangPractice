f459a7d183a8d8c23a78efd40ac14cd26e805aa6
SashaY <yehat@appstate.edu>
2023-10-25 13:39:22 -0400

    List of floating point integers for n^2 from 0 to 20

---
+++ summation.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+	printf("Floating Point Integers of Sequence n^2\n");
+
+	int i;
+
+	for (i = 1; i <= 20; i++ )
+	printf("%f\n", pow(i,2));
+
+
+
+
+
+}
+		
